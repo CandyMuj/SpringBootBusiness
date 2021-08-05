f2648c2d14d9ec46a3f35bac6bf33f0ab61a8d3f
CandyMuj <jx003js@gmail.com>
2021-08-05 14:26:11 +0800

    新增 SrcMethod.java

---
+++ src/main/java/com/cc/pic/api/src/utils/SrcMethod.java
@@ -0,0 +1,16 @@
+package com.cc.pic.api.src.utils;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * @ProjectName api
+ * @FileName SrcMethod
+ * @Description
+ * @Author CandyMuj
+ * @Date 2021/8/5 14:25
+ * @Version 1.0
+ */
+@Slf4j
+public class SrcMethod {
+
+}
