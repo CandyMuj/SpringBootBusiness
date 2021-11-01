16c0fc909ad3578bae078a6cad30787b7312e1b1
Jun <jx003js@gmail.com>
2021-11-01 16:59:39 +0800

    开启定时任务

--- src/main/java/com/cc/pic/api/ApiserverApplication.java
+++ src/main/java/com/cc/pic/api/ApiserverApplication.java
@@ -4,12 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
 
 @EnableAsync
+@EnableScheduling
 @SpringBootApplication
 @EnableTransactionManagement
 //@ComponentScan(basePackages = {"com.cc.pic.api"})
