ab068385b8e8125d43633be321cd7cc2293e5d38
Jun <jx003js@gmail.com>
2022-02-08 18:01:30 +0800

    新增：SpringActive类，获取当前程序运行的active环境

---
+++ candy-core/src/main/java/com/cc/api/config/sys/init/SpringInitConstruct.java
@@ -0,0 +1,31 @@
+package com.cc.api.config.sys.init;
+
+import com.cc.api.config.SpringActive;
+import org.springframework.core.env.Environment;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.Resource;
+
+/**
+ * @ProjectName SpringBootBusiness
+ * @FileName SpringConstruct
+ * @Description 在springboot启动时，进行的一些系统初始执行的逻辑
+ * PostConstruct的方式初始化: @PostConstruct注解的方法将会在依赖注入完成后被自动调用。
+ * @Author CandyMuj
+ * @Date 2022/2/8 17:53
+ * @Version 1.0
+ */
+@Component
+public class SpringInitConstruct {
+    @Resource
+    private Environment environment;
+
+
+    @PostConstruct
+    private void initSpringActive() {
+        // 初始化环境配置文件
+        SpringActive.init(environment.getActiveProfiles()[0]);
+    }
+
+}
