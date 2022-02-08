6aedf1f4158013dd76d5f5bbdabcf4ce3a8b2132
Jun <jx003js@gmail.com>
2022-02-08 18:09:16 +0800

    [sync]新增：SpringInitCommandLine类，进行一些初始化操作（后期若有需要则再扩展）

---
+++ src/main/java/com/cc/pic/api/config/sys/init/SpringInitCommandLine.java
@@ -0,0 +1,30 @@
+package com.cc.pic.api.config.sys.init;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+/**
+ * @ProjectName SpringBootBusiness
+ * @FileName SpringInitCommandLine
+ * @Description 在springboot启动时，进行的一些系统初始执行的逻辑
+ * CommandLineRunner的方式初始化: 在程序启动成功后调用一次实现了CommandLineRunner接口的run方法，通过order=1指定最先执此类（默认值是一个很大的值，可以点order进去看）
+ * @Author CandyMuj
+ * @Date 2022/2/8 18:01
+ * @Version 1.0
+ */
+@Slf4j
+@Order(1)
+@Component
+public class SpringInitCommandLine implements CommandLineRunner {
+
+
+    @Override
+    public void run(String... args) throws Exception {
+        log.info("系统初始化...");
+
+        log.info("系统初始化 完成!");
+    }
+
+}
