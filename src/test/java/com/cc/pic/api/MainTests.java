bbbb53df9d3c14cbe5e040d1966cdfabd53dcbfb
Jun <jx003js@gmail.com>
2023-02-08 16:53:57 +0800

    [优化]添加单元测试类并移除默认的单元测试类

---
+++ src/test/java/com/cc/pic/api/MainTests.java
@@ -0,0 +1,18 @@
+package com.cc.pic.api;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * @Description
+ * @Author CandyMuj
+ * @Date 2023/2/2 16:46
+ * @Version 1.0
+ */
+public class MainTests {
+
+    @Test
+    public void test() {
+
+    }
+
+}
