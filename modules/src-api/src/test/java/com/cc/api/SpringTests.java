258c60e95bb13e8e2040792f1137fe74744da631
Jun <jx003js@gmail.com>
2023-02-09 10:29:34 +0800

    [优化]添加单元测试类并移除默认的单元测试类

---
+++ modules/src-api/src/test/java/com/cc/api/SpringTests.java
@@ -0,0 +1,23 @@
+package com.cc.api;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * @Description
+ * @Author CandyMuj
+ * @Date 2023/2/2 16:46
+ * @Version 1.0
+ */
+@Slf4j
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class SpringTests {
+
+
+    @Test
+    public void test() {
+
+    }
+
+}
