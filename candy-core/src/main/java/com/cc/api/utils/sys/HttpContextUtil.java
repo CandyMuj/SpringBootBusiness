8e6845b8e3b8aee467e023f2fcd6b9858fa3d12b
Jun <jx003js@gmail.com>
2023-02-09 10:16:17 +0800

    [优化]添加HttpContextUtil工具类 直接获取request；并修改之前使用RequestContextHolder的地方改为此工具中的getHttpServletRequest方法

---
+++ candy-core/src/main/java/com/cc/api/utils/sys/HttpContextUtil.java
@@ -0,0 +1,28 @@
+package com.cc.api.utils.sys;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * @Description http相关的工具
+ * @Author CandyMuj
+ * @Date 2023/2/3 14:08
+ * @Version 1.0
+ */
+@Slf4j
+public class HttpContextUtil {
+
+    public static HttpServletRequest getHttpServletRequest() {
+        try {
+            ServletRequestAttributes attributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+            if (attributes == null) return null;
+            return attributes.getRequest();
+        } catch (Exception e) {
+            return null;
+        }
+    }
+
+}
